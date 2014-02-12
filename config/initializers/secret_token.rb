0f5963262a41238c8a67fb9a7f34d285d5d557e6
Dickson Tse <dtse87@gmail.com>
2014-02-11 22:27:08 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Devfolio::Application.config.secret_key_base = '6463a5283062d683f1723d28b30f9fe018db4c5df15505e354a3262083bd8a7ade6db0efdbb53644491255091aa5b5e1a4f43f5c9edc80f5c987540de6da0db8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Devfolio::Application.config.session_store :cookie_store, key: '_devfolio_session'
