08475434b4179be99d13884df6fdef7a4ff91bae
Dickson Tse <dtse87@gmail.com>
2014-03-10 00:23:12 -0700

    Fix User model to use strong parameters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,2 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :username, :password, :password_confirmation
-                  :full_name, :location, :website, :github
-  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 20140307071342) do
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
