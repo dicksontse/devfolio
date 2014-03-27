51bbcf19372ac637a30f1e463d2a755de21b037c
Dickson Tse <dtse87@gmail.com>
2014-03-26 20:44:26 -0700

    Add password_digest to Users and add basic validations on Sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true, length: { maximum: 50 }
+  validates :email, presence: true
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
