bbfb6b5212c773df72d07f8761e4aaaf54918bb8
Dickson Tse <dtse87@gmail.com>
2014-03-31 22:40:29 -0700

    Add basic sign in logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Sign in and redirect to user's show page
+    else
+      # Error
+      render 'new'
+    end
   end
 
   def destroy
