620a02537bc0952ab0b3cbab7c43ecdac05aceff
Dickson Tse <dtse87@gmail.com>
2014-04-20 19:54:19 -0700

    Fix password validation for create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   validates :username, presence: true, length: { maximum: 50 }
   validates :email, presence: true
   has_secure_password
-  #validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, :on => :create
+  validates :password, length: { minimum: 6 }, allow_blank: true
 
   before_save { self.email = email.downcase }
   before_create :create_remember_token
