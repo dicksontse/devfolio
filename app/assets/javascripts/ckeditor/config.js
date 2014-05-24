74660354f28eeca668a95724d1141eff591bb795
Dickson Tse <dtse87@gmail.com>
2014-05-23 23:04:39 -0700

    Add Source button to ckeditor toolbar

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -5,6 +5,8 @@ CKEDITOR.editorConfig = function( config )
   config.toolbar_Mini =
     [
         ['Cut','Copy','Paste',], ['Undo','Redo'],
-        ['Bold','Italic','Underline','Strike'], ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote']
+        ['Bold','Italic','Underline','Strike'], 
+        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+        ['Source']
     ];
 }
