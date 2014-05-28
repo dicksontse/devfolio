084ebcdd05fe3468780f99d3db475692da8ea1f3
Dickson Tse <dtse87@gmail.com>
2014-05-27 21:21:52 -0700

    Fix ckeditor integration

---
+++ public/assets/ckeditor/config-836b1754f119adf7c51e9e4393bf4b34.js
@@ -0,0 +1,13 @@
+CKEDITOR.editorConfig = function( config )
+{
+  config.extraAllowedContent = ["section"];
+  config.toolbar = 'Mini';
+  config.toolbar_Mini =
+    [
+        ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat'],
+        ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+        ['Link', 'Unlink'], ['Source'],
+        ['Format', 'Font', 'FontSize'], ['TextColor', 'BGColor']
+    ];
+}
+;
