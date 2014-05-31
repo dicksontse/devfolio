525fdf5cea236d221f2e10938af6a5be8751461c
Dickson Tse <dtse87@gmail.com>
2014-05-30 22:01:51 -0700

    Add gravatar attribute to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140502052412) do
+ActiveRecord::Schema.define(version: 20140531050009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20140502052412) do
     t.text     "experience_section"
     t.text     "projects_section"
     t.text     "skills_section"
+    t.boolean  "gravatar"
   end
 
   add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
