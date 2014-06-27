f31d0bdf2c6093adf94671a90b5124bba6c6e095
Dickson Tse <dtse87@gmail.com>
2014-06-26 22:07:57 -0700

    Validate for uniqueness on :username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :username, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, uniqueness: true, length: { maximum: 50 }
   validates :email, presence: true
   has_secure_password
   validates :password, length: { minimum: 6 }, :on => :create
